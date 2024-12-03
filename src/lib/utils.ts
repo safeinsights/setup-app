f4516933b99ff154eb5180cbd4ca849e5fd3bb30
Ripal Nathuji <rnathuji@neoteklabs.com>
2024-12-03 09:21:25 -0600

    Add utility function to identify orphan task defs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,7 @@
 import jwt from 'jsonwebtoken'
 import { ManagementAppGetRunnableStudiesResponse, TOAGetRunsResponse } from './types'
+import { ResourceTagMapping } from '@aws-sdk/client-resource-groups-tagging-api'
+import { RUN_ID_TAG_KEY } from './aws'
 
 // Functions for interacting with the Management App
 const generateToken = (): string => {
@@ -64,4 +66,23 @@ export const filterManagmentAppRuns = (
             return !runIdArray.includes(run.runId) && !existingAwsRuns.includes(run.runId)
         }),
     }
+}
+
+export const filterOrphanTaskDefinitions = (
+    managementAppResponse: ManagementAppGetRunnableStudiesResponse,
+    taskDefinitionResources: ResourceTagMapping[],
+): string[] => {
+    const managementAppRunIds = managementAppResponse.runs.map((run) => run.runId)
+    const orphanTaskDefinitions: string[] = []
+
+    // If there are task definitions with run IDs that were not returned by
+    // the BMA, they must be for orphan tasks that were already run.
+    taskDefinitionResources.forEach((item) => {
+        const runId = item.Tags?.find((tag) => tag.Key === RUN_ID_TAG_KEY)?.Value
+        if (runId !== undefined && !managementAppRunIds.includes(runId) && item.ResourceARN !== undefined) {
+            orphanTaskDefinitions.push(item.ResourceARN)
+        }
+    })
+
+    return orphanTaskDefinitions
 }
