d89432251c1d58da5a4148014e69331157dd2fd1
KC Swanson <kendraswan@gmail.com>
2024-12-13 10:44:39 -0500

    split api functions into separate file

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,92 +1,8 @@
 import { describe, it, expect, beforeEach, vi } from 'vitest'
 import {
-    managementAppGetRunnableStudiesRequest,
-    toaGetRunsRequest,
-    filterManagmentAppRuns,
+    filterManagementAppRuns,
     filterOrphanTaskDefinitions,
 } from '../lib/utils'
-import jwt from 'jsonwebtoken'
-
-beforeEach(() => {
-    process.env.MANAGEMENT_APP_PRIVATE_KEY = 'mockprivatekeyvalue'
-    process.env.MANAGEMENT_APP_BASE_URL = 'http://bma:12345'
-    process.env.MANAGEMENT_APP_MEMBER_ID = 'openstax'
-    process.env.TOA_BASE_URL = 'http://toa:67890'
-    process.env.TOA_BASIC_AUTH = 'testusername:testpassword'
-})
-
-describe('managementAppGetRunnableStudiesRequest', () => {
-    it('should generate a token and make a GET request', async () => {
-        const mockSignToken = vi.fn().mockReturnValue('mocktokenvalue')
-        const mockStudiesData = {
-            runs: [
-                {
-                    runId: 'runId1',
-                    studyId: 'studyId1',
-                    containerLocation: 'repo1:tag1',
-                },
-                {
-                    runId: 'runId2',
-                    studyId: 'studyId2',
-                    containerLocation: 'repo1:tag2',
-                },
-            ],
-        }
-        global.fetch = vi.fn().mockResolvedValue({
-            json: async () => await Promise.resolve(mockStudiesData),
-        })
-        vi.spyOn(jwt, 'sign').mockImplementation(mockSignToken)
-
-        const result = await managementAppGetRunnableStudiesRequest()
-
-        expect(mockSignToken).toHaveBeenCalledOnce()
-        expect(mockSignToken).toHaveBeenCalledWith({ iss: 'openstax' }, 'mockprivatekeyvalue', { algorithm: 'RS256' })
-        expect(global.fetch).toHaveBeenCalledOnce()
-        expect(global.fetch).toHaveBeenCalledWith('http://bma:12345/api/studies/runnable', {
-            method: 'GET',
-            headers: {
-                Authorization: 'Bearer mocktokenvalue',
-                'Content-Type': 'application/json',
-            },
-        })
-        expect(result).toEqual(mockStudiesData)
-    })
-
-    it('should throw an error if the token generation fails', async () => {
-        const mockSignToken = vi.fn().mockReturnValue('')
-        vi.spyOn(jwt, 'sign').mockImplementation(mockSignToken)
-
-        await expect(managementAppGetRunnableStudiesRequest()).rejects.toThrow('Managment App token failed to generate')
-    })
-})
-
-describe('toaGetRunsRequest', () => {
-    it('should make a GET request', async () => {
-        const mockTOAData = {
-            runs: [{ runId: '1234' }],
-        }
-        global.fetch = vi.fn().mockResolvedValue({
-            json: async () => await Promise.resolve(mockTOAData),
-        })
-
-        const result = await toaGetRunsRequest()
-
-        const mockToken = Buffer.from('testusername:testpassword').toString('base64')
-        expect(global.fetch).toHaveBeenCalledWith('http://toa:67890/api/runs', {
-            method: 'GET',
-            headers: {
-                Authorization: `Basic ${mockToken}`,
-                'Content-Type': 'application/json',
-            },
-        })
-        expect(result).toEqual(mockTOAData)
-    })
-
-    it('should error if token not found', async () => {
-        process.env.TOA_BASIC_AUTH = ''
-        await expect(toaGetRunsRequest()).rejects.toThrow('TOA token failed to generate')
-    })
-})
 
 describe('filterManagementAppRuns', () => {
     it('filters out runs in the TOA', () => {
@@ -105,7 +21,7 @@ describe('filterManagementAppRuns', () => {
             ],
         }
         const mockTOAResponse = { runs: [{ runId: 'finished-run' }] }
-        expect(filterManagmentAppRuns(mockManagementAppResponse, mockTOAResponse, [])).toStrictEqual({
+        expect(filterManagementAppRuns(mockManagementAppResponse, mockTOAResponse, [])).toStrictEqual({
             runs: [
                 {
                     runId: 'not-in-TOA',
@@ -130,7 +46,7 @@ describe('filterManagementAppRuns', () => {
                 },
             ],
         }
-        expect(filterManagmentAppRuns(mockManagementAppResponse, { runs: [] }, ['existing-run'])).toStrictEqual({
+        expect(filterManagementAppRuns(mockManagementAppResponse, { runs: [] }, ['existing-run'])).toStrictEqual({
             runs: [
                 {
                     runId: 'not-in-AWS',
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,59 +1,9 @@
-import jwt from 'jsonwebtoken'
 import { ManagementAppGetRunnableStudiesResponse, TOAGetRunsResponse } from './types'
 import { ResourceTagMapping } from '@aws-sdk/client-resource-groups-tagging-api'
 import { RUN_ID_TAG_KEY } from './aws'
 
-// Functions for interacting with the Management App
-const generateToken = (): string => {
-    const privateKey: string | undefined = process.env.MANAGEMENT_APP_PRIVATE_KEY
-    const memberId = process.env.MANAGEMENT_APP_MEMBER_ID
-
-    let token = ''
-    if (privateKey && memberId) {
-        token = jwt.sign({ iss: memberId }, privateKey, { algorithm: 'RS256' })
-    }
-    if (token.length === 0) {
-        throw new Error('Managment App token failed to generate')
-    }
-    return token
-}
-
-export const managementAppGetRunnableStudiesRequest = async (): Promise<ManagementAppGetRunnableStudiesResponse> => {
-    const endpoint = process.env.MANAGEMENT_APP_BASE_URL + '/api/studies/runnable' // `http://localhost:4000/api/studies/runnable`
-    const token = generateToken()
-    const response = await fetch(endpoint, {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
-        },
-    })
-
-    const data = await response.json()
-    return data
-}
-
-// Functions for interacting with the Trusted Output App
-export const toaGetRunsRequest = async (): Promise<TOAGetRunsResponse> => {
-    const endpoint = process.env.TOA_BASE_URL + '/api/runs' // `http://localhost:3002/api/runs`
-    const token = Buffer.from(process.env.TOA_BASIC_AUTH || '').toString('base64')
-    if (token.length === 0) {
-        throw new Error('TOA token failed to generate')
-    }
-
-    const response = await fetch(endpoint, {
-        method: 'GET',
-        headers: {
-            Authorization: `Basic ${token}`,
-            'Content-Type': 'application/json',
-        },
-    })
-    const data = await response.json()
-    return data
-}
-
 // Other
-export const filterManagmentAppRuns = (
+export const filterManagementAppRuns = (
     managementAppResponse: ManagementAppGetRunnableStudiesResponse,
     toaResponse: TOAGetRunsResponse,
     existingAwsRuns: string[],
