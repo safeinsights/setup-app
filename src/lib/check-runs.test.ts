b828d2566b2c39b0a30878a7d51ce03e6339ee82
Ripal Nathuji <rnathuji@neoteklabs.com>
2025-02-05 15:26:59 -0600

    Add function that polls AWS for errored tasks

---
+++ src/lib/check-runs.test.ts
@@ -0,0 +1,182 @@
+import { vi, describe, it, expect } from 'vitest'
+import { checkForErroredRuns } from './check-runs'
+import * as api from './api'
+import * as aws from './aws'
+import { ECSClient, TaskStopCode } from '@aws-sdk/client-ecs'
+
+vi.mock('./api')
+vi.mock('./aws')
+
+describe('checkForErroredRuns()', () => {
+    it('exits early if there are no tasks with runId tag', async () => {
+        vi.mocked(aws.getAllTasksWithRunId).mockResolvedValue([])
+
+        const mockDescribeECSTasks = vi.mocked(aws.describeECSTasks)
+        await checkForErroredRuns()
+
+        expect(mockDescribeECSTasks).not.toHaveBeenCalled()
+    })
+    it('makes call to TOA for task that fails to start', async () => {
+        vi.mocked(aws.getAllTasksWithRunId).mockResolvedValue([
+            {
+                ResourceARN: 'arn1',
+            },
+            {
+                ResourceARN: 'arn2',
+            },
+        ])
+
+        const mockDescribeECSTasks = vi.mocked(aws.describeECSTasks)
+        const mockToaUpdateRunStatus = vi.mocked(api.toaUpdateRunStatus)
+        const mockTask = {
+            stopCode: TaskStopCode.TASK_FAILED_TO_START,
+            tags: [
+                {
+                    key: aws.RUN_ID_TAG_KEY,
+                    value: 'runId1',
+                },
+            ],
+        }
+        mockDescribeECSTasks.mockResolvedValue({
+            tasks: [mockTask],
+            $metadata: {},
+        })
+
+        await checkForErroredRuns()
+
+        expect(mockDescribeECSTasks).toHaveBeenCalledOnce()
+        expect(mockDescribeECSTasks).toHaveBeenCalledWith(expect.any(ECSClient), 'MOCK_ECS_CLUSTER', ['arn1', 'arn2'])
+        expect(mockToaUpdateRunStatus).toHaveBeenCalledOnce()
+        expect(mockToaUpdateRunStatus).toHaveBeenLastCalledWith('runId1', {
+            status: 'ERRORED',
+            message: 'Task failed to start',
+        })
+    })
+    it('makes call to TOA for task that exits with non-zero exit code', async () => {
+        vi.mocked(aws.getAllTasksWithRunId).mockResolvedValue([
+            {
+                ResourceARN: 'arn1',
+            },
+            {
+                ResourceARN: 'arn2',
+            },
+        ])
+
+        const mockDescribeECSTasks = vi.mocked(aws.describeECSTasks)
+        const mockToaUpdateRunStatus = vi.mocked(api.toaUpdateRunStatus)
+        const mockTask = {
+            containers: [{ exitCode: 1 }],
+            stopCode: TaskStopCode.ESSENTIAL_CONTAINER_EXITED,
+            tags: [
+                {
+                    key: aws.RUN_ID_TAG_KEY,
+                    value: 'runId1',
+                },
+            ],
+        }
+        mockDescribeECSTasks.mockResolvedValue({
+            tasks: [mockTask],
+            $metadata: {},
+        })
+
+        await checkForErroredRuns()
+
+        expect(mockDescribeECSTasks).toHaveBeenCalledOnce()
+        expect(mockDescribeECSTasks).toHaveBeenCalledWith(expect.any(ECSClient), 'MOCK_ECS_CLUSTER', ['arn1', 'arn2'])
+        expect(mockToaUpdateRunStatus).toHaveBeenCalledOnce()
+        expect(mockToaUpdateRunStatus).toHaveBeenLastCalledWith('runId1', {
+            status: 'ERRORED',
+            message: 'Task container stopped with non-zero exit code',
+        })
+    })
+    it('does not make call to TOA for task that exits with zero exit code', async () => {
+        vi.mocked(aws.getAllTasksWithRunId).mockResolvedValue([
+            {
+                ResourceARN: 'arn1',
+            },
+            {
+                ResourceARN: 'arn2',
+            },
+        ])
+
+        const mockDescribeECSTasks = vi.mocked(aws.describeECSTasks)
+        const mockToaUpdateRunStatus = vi.mocked(api.toaUpdateRunStatus)
+        const mockTask = {
+            containers: [{ exitCode: 0 }],
+            stopCode: TaskStopCode.ESSENTIAL_CONTAINER_EXITED,
+            tags: [
+                {
+                    key: aws.RUN_ID_TAG_KEY,
+                    value: 'runId1',
+                },
+            ],
+        }
+        mockDescribeECSTasks.mockResolvedValue({
+            tasks: [mockTask],
+            $metadata: {},
+        })
+
+        await checkForErroredRuns()
+
+        expect(mockDescribeECSTasks).toHaveBeenCalledOnce()
+        expect(mockDescribeECSTasks).toHaveBeenCalledWith(expect.any(ECSClient), 'MOCK_ECS_CLUSTER', ['arn1', 'arn2'])
+        expect(mockToaUpdateRunStatus).not.toHaveBeenCalled()
+    })
+    it('behaves gracefully when describeECSTasks returns undefined tasks', async () => {
+        vi.mocked(aws.getAllTasksWithRunId).mockResolvedValue([
+            {
+                ResourceARN: 'arn1',
+            },
+            {
+                ResourceARN: 'arn2',
+            },
+        ])
+
+        const mockDescribeECSTasks = vi.mocked(aws.describeECSTasks)
+        const mockToaUpdateRunStatus = vi.mocked(api.toaUpdateRunStatus)
+
+        mockDescribeECSTasks.mockResolvedValue({
+            tasks: undefined,
+            $metadata: {},
+        })
+
+        await checkForErroredRuns()
+
+        expect(mockDescribeECSTasks).toHaveBeenCalledOnce()
+        expect(mockDescribeECSTasks).toHaveBeenCalledWith(expect.any(ECSClient), 'MOCK_ECS_CLUSTER', ['arn1', 'arn2'])
+        expect(mockToaUpdateRunStatus).not.toHaveBeenCalled()
+    })
+    it('behaves gracefully if stopped task has no container data', async () => {
+        vi.mocked(aws.getAllTasksWithRunId).mockResolvedValue([
+            {
+                ResourceARN: 'arn1',
+            },
+            {
+                ResourceARN: 'arn2',
+            },
+        ])
+
+        const mockDescribeECSTasks = vi.mocked(aws.describeECSTasks)
+        const mockToaUpdateRunStatus = vi.mocked(api.toaUpdateRunStatus)
+        const mockTask = {
+            containers: undefined,
+            stopCode: TaskStopCode.ESSENTIAL_CONTAINER_EXITED,
+            tags: [
+                {
+                    key: aws.RUN_ID_TAG_KEY,
+                    value: 'runId1',
+                },
+            ],
+        }
+        mockDescribeECSTasks.mockResolvedValue({
+            tasks: [mockTask],
+            $metadata: {},
+        })
+
+        await checkForErroredRuns()
+
+        expect(mockDescribeECSTasks).toHaveBeenCalledOnce()
+        expect(mockDescribeECSTasks).toHaveBeenCalledWith(expect.any(ECSClient), 'MOCK_ECS_CLUSTER', ['arn1', 'arn2'])
+        expect(mockToaUpdateRunStatus).not.toHaveBeenCalled()
+    })
+})
---
+++ src/lib/check-runs.ts
@@ -0,0 +1,60 @@
+import { toaUpdateRunStatus } from '../lib/api'
+import { describeECSTasks, getAllTasksWithRunId, RUN_ID_TAG_KEY } from '../lib/aws'
+import { ensureValueWithError } from '../lib/utils'
+import { ECSClient, TaskStopCode } from '@aws-sdk/client-ecs'
+import { ResourceGroupsTaggingAPIClient } from '@aws-sdk/client-resource-groups-tagging-api'
+import 'dotenv/config'
+
+export async function checkForErroredRuns(): Promise<void> {
+    /**
+     * We check for errored runs exclusively using AWS state since the PUT semantics
+     * of updating status via TOA should be idempotent. This reduces load on the BMA
+     * while also keeping the logic here extremely simple
+     */
+    const ecsClient = new ECSClient()
+    const taggingClient = new ResourceGroupsTaggingAPIClient()
+    const cluster = ensureValueWithError(process.env.ECS_CLUSTER, 'Env var ECS_CLUSTER not found')
+
+    const tasks = await getAllTasksWithRunId(taggingClient)
+
+    const taskArns: string[] = []
+
+    tasks.forEach((task) => {
+        if (task.ResourceARN !== undefined) {
+            taskArns.push(task.ResourceARN)
+        }
+    })
+
+    if (taskArns.length == 0) {
+        // There are no tasks to look into
+        return
+    }
+
+    const data = await describeECSTasks(ecsClient, cluster, taskArns)
+
+    for (const run of data.tasks ?? []) {
+        const maybeRunId = run.tags?.find((tag) => tag.key === RUN_ID_TAG_KEY)?.value
+        const runId = ensureValueWithError(maybeRunId, 'Could not find runId in task tags')
+
+        if (run.stopCode === TaskStopCode.TASK_FAILED_TO_START) {
+            // NOTE: While run.stopReason provides useful information for devs / operators,
+            // for now we're not passing that through since the AWS generated message may
+            // be confusing to users
+            await toaUpdateRunStatus(runId, { status: 'ERRORED', message: 'Task failed to start' })
+        }
+
+        if (run.stopCode === TaskStopCode.ESSENTIAL_CONTAINER_EXITED) {
+            // We only expect one container in practice today, but the following will
+            // set a status error if any container in the task has a non-zero exit code
+            for (const container of run.containers ?? []) {
+                const maybeExitCode = container.exitCode
+                if (maybeExitCode !== undefined && maybeExitCode !== 0) {
+                    await toaUpdateRunStatus(runId, {
+                        status: 'ERRORED',
+                        message: 'Task container stopped with non-zero exit code',
+                    })
+                }
+            }
+        }
+    }
+}
