3bc59eeedaa9d0a15cac46e6ebe30811dcaf068f
Samuel Klutse <klutse.samuel@gmail.com>
2025-03-04 12:27:28 -0500

    Fixed the ServiceAccount test issues

--- src/lib/kube.test.ts
+++ src/lib/kube.test.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs'
 import { beforeEach, describe, expect, it } from 'vitest'
 import { DEFAULT_SERVICE_ACCOUNT_PATH, getKubeAPIServiceAccountToken, getNamespace, initHTTPSTrustStore } from './kube'
 
-describe('getNamespace', () => {
+describe('Get Service Account items', () => {
     beforeEach(async () => {
         process.env.K8S_SERVICEACCOUNT_PATH = getServiceAccountDir()
     })
@@ -20,31 +20,29 @@ describe('getNamespace', () => {
         expect(fs.existsSync(filePath)).toBe(false)
         expect(() => getNamespace()).toThrow(`Namespace file not found at ${filePath}`)
     })
-})
 
-describe('getKubeAPIServiceAccountToken', () => {
-    const filePath = `${process.env.K8S_SERVICEACCOUNT_PATH}/token`
     it('should read token from file and return trimmed value', () => {
+        const filePath = `${process.env.K8S_SERVICEACCOUNT_PATH}/token`
         expect(fs.existsSync(filePath)).toBe(true)
         const result = getKubeAPIServiceAccountToken()
         expect(result).toBe('mock-token')
     })
 
     it('should throw an error if the token file does not exist', () => {
+        const filePath = `${DEFAULT_SERVICE_ACCOUNT_PATH}/token`
         delete process.env.K8S_SERVICEACCOUNT_PATH
         expect(fs.existsSync(filePath)).toBe(false)
         expect(() => getKubeAPIServiceAccountToken()).toThrow(`Token file not found at ${filePath}`)
     })
-})
 
-describe('initHTTPSTrustStore', () => {
-    const filePath = `${process.env.K8S_SERVICEACCOUNT_PATH}/ca.crt`
     it('should initialize the https trust store', () => {
+        const filePath = `${process.env.K8S_SERVICEACCOUNT_PATH}/ca.crt`
         expect(fs.existsSync(filePath)).toBe(true)
         expect(() => initHTTPSTrustStore).not.toThrow()
     })
 
     it('should throw an error if the certificate file does not exist', () => {
+        const filePath = `${DEFAULT_SERVICE_ACCOUNT_PATH}/ca.crt`
         delete process.env.K8S_SERVICEACCOUNT_PATH
         expect(fs.existsSync(filePath)).toBe(false)
         expect(() => initHTTPSTrustStore()).toThrow(`Certificate file not found at ${filePath}`)
