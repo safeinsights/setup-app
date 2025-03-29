1dc777453554698e180c4afff5338dac949e16ee
Samuel Klutse <klutse.samuel@gmail.com>
2025-03-29 18:31:32 -0400

    Added docker enclave implementation

---
+++ src/lib/docker-enclave.ts
@@ -0,0 +1,91 @@
+import { createContainerObject, dockerApiCall, filterContainers, pullContainer } from './docker'
+import { Enclave, IEnclave } from './enclave'
+import {
+    DockerApiContainersResponse,
+    DockerApiResponse,
+    ManagementAppGetReadyStudiesResponse,
+    ManagementAppJob,
+    TOAGetJobsResponse,
+} from './types'
+
+class DockerEnclave extends Enclave<DockerApiContainersResponse> implements IEnclave<DockerApiContainersResponse> {
+    filterJobsInEnclave(
+        bmaReadysResults: ManagementAppGetReadyStudiesResponse,
+        toaGetJobsResult: TOAGetJobsResponse,
+        runningJobsInEnclave: DockerApiContainersResponse[],
+    ): ManagementAppGetReadyStudiesResponse {
+        if (!runningJobsInEnclave?.length) return bmaReadysResults || {jobs:[]}
+        
+        return {jobs: bmaReadysResults.jobs.filter(job=> toaGetJobsResult.jobs.map(r=>r.jobId).includes(job.jobId))}
+    }
+
+    async cleanup(): Promise<void> {
+        console.log("Cleaning up the enclave. Removing successfully rand studies.")
+
+        const successfulContainers = filterContainers(
+            await this.getAllStudiesInEnclave(),
+            {
+                component: 'research-container',
+                'managed-by': 'setup-app',
+            },
+            'completed',
+        )
+        successfulContainers.forEach(async container=>{
+            console.log(`Removing Container ${container.Id}`)
+            try{
+                await dockerApiCall('DEL', `containers/${container.Id}`)
+            }catch (error: unknown) {
+                const err = error as Error & { cause: string }
+                console.error(`Error deleting container: [${container.Id}]. Please check the logs for more details. Cause: ${err['cause']}`)
+            }
+        })
+    }
+
+    async getAllStudiesInEnclave(): Promise<DockerApiContainersResponse[]> {
+        try {
+            const containers: DockerApiResponse = await dockerApiCall('GET', 'containers/json')
+            console.log(`Pulled the following containers: ${JSON.stringify(containers)}`)
+            return containers as DockerApiContainersResponse[]
+        } catch (error: unknown) {
+            const err = error as Error & { cause: string }
+            console.error(`Error getting containers. Please check the logs for more details. Cause: ${err['cause']}`)
+        }
+        return []
+    }
+    async getRunningStudies(): Promise<DockerApiContainersResponse[]> {
+        console.log('Docker => Retrieving running research containers')
+        return filterContainers(
+            await this.getAllStudiesInEnclave(),
+            {
+                component: 'research-container',
+                'managed-by': 'setup-app',
+            },
+            'running',
+        )
+    }
+    async launchStudy(job: ManagementAppJob, toaEndpointWithJobId: string): Promise<void> {
+        const container = createContainerObject(job.containerLocation, job.jobId, job.title)
+        console.log(`Deploying Container ==> ${JSON.stringify(container)}`)
+        try {
+            const result = await pullContainer(job.containerLocation)
+            console.log(`Pull result ${JSON.stringify(result)}`)
+            const path = `containers/create?name=rc-${job.jobId}`
+            const response: DockerApiResponse = await dockerApiCall('POST', path, container)
+            if ('Id' in response) {
+                await dockerApiCall(
+                    'POST',
+                    `containers/${response.Id}/start`,
+                    undefined,
+                    true,
+                )
+            }
+            console.log(`Successfully deployed ${job.title} with run id ${job.jobId}`)
+        } catch (error: unknown) {
+            const errMsg = `Docker API Call Error: Failed to deploy ${job.title} with run id ${job.jobId}. Cause: ${JSON.stringify(error)}`
+            console.error(errMsg)
+            throw new Error(errMsg)
+        }
+    }
+}
+
+export { DockerEnclave }
--- src/lib/run-studies.ts
+++ src/lib/run-studies.ts
@@ -1,13 +1,16 @@
 import { runAWSStudies } from './aws-run-studies'
 import { runK8sStudies } from './kube-run-studies'
+import { DockerEnclave } from './docker-enclave'
 export async function runStudies(options: { ignoreAWSJobs: boolean }): Promise<void> {
     const deploymentEnvironment = process.env.DEPLOYMENT_ENVIRONMENT ?? 'AWS' /* v8 ignore next */
     if (deploymentEnvironment === 'AWS') {
         await runAWSStudies(options)
     } else if (deploymentEnvironment === 'KUBERNETES') {
-        // Implement K8S study execution here
-        console.log('Running studies in Kubernetes environment')
+        console.log('Running Setup App in Kubernetes environment')
         await runK8sStudies()
+    } else if (deploymentEnvironment === 'DOCKER') {
+        console.log('Running Setup App in Docker environment')
+        await new DockerEnclave().runStudies()
     } else {
         throw new Error(`Unsupported deployment environment: ${deploymentEnvironment}`)
     }
